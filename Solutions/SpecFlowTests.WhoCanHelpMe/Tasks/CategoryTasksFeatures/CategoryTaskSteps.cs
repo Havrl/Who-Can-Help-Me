b9268cce757580835e08d15ebddb290a90a4b6ec
Jonathan George <jon@jonathangeorge.co.uk>
2010-08-17 09:55:01 +0100

    First spec implemented in SpecFlow

---
+++ Solutions/SpecFlowTests.WhoCanHelpMe/Tasks/CategoryTasksFeatures/CategoryTaskSteps.cs
@@ -0,0 +1,106 @@
+﻿namespace SpecFlowTests.WhoCanHelpMe.Tasks.CategoryTasksFeatures
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using global::WhoCanHelpMe.Domain;
+    using global::WhoCanHelpMe.Domain.Contracts.Repositories;
+    using global::WhoCanHelpMe.Domain.Contracts.Tasks;
+    using global::WhoCanHelpMe.Tasks;
+
+    using NUnit.Framework;
+
+    using Rhino.Mocks;
+
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class CategoryTaskSteps
+    {
+        private ICategoryRepository categoryRepository;
+        
+        private ICategoryTasks subject;
+        
+        private IList<Category> getAllResult;
+        
+        private Category getOneResult;
+
+        private List<Category> theCategories;
+
+        private Category theCategory;
+
+        [Given(@"I have category tasks available")]
+        public void GivenIHaveCategoryTasksAvailable()
+        {
+            this.categoryRepository = MockRepository.GenerateMock<ICategoryRepository>();
+            this.subject = new CategoryTasks(this.categoryRepository);
+        }
+
+        [Given(@"there are (.*) categories in the repository")]
+        public void GivenThereAreCategoriesInTheRepository(int categories)
+        {
+            this.theCategories = new List<Category>();
+
+            for (int index = 0; index < categories; index++)
+            {
+                this.theCategories.Add(new Category());
+            }
+
+            this.categoryRepository.Stub(r => r.FindAll()).Return(this.theCategories.AsQueryable());
+        }
+
+        [Given(@"there is a category with Id (.*) in the repository")]
+        public void GivenThereIsACategoryWithIdInTheRepository(int id)
+        {
+            this.theCategory = new Category();
+            this.categoryRepository.Stub(r => r.FindOne(id)).Return(this.theCategory);
+        }
+
+        [Given(@"there is no category with Id (.*) in the repository")]
+        public void GivenThereIsNoCategoryWithIdInTheRepository(int id)
+        {
+            this.categoryRepository.Stub(r => r.FindOne(id)).Return(null);
+        }
+
+        [When(@"I ask for all categories")]
+        public void WhenIAskForAllCategories()
+        {
+            this.getAllResult = this.subject.GetAll();
+        }
+
+        [When(@"I ask for the category with Id (.*)")]
+        public void WhenIAskForTheCategoryWithId(int id)
+        {
+            this.getOneResult = this.categoryRepository.FindOne(id);
+        }
+
+        [Then(@"the list of all categories should be returned")]
+        public void ThenTheListOfAllCategoriesShouldBeReturned()
+        {
+            Assert.That(this.getAllResult.Count, Is.EqualTo(3));
+            
+            foreach (var current in this.theCategories)
+            {
+                Assert.That(this.getAllResult, Contains.Item(current));
+            }
+        }
+
+        [Then(@"an empty list should be returned")]
+        public void ThenAnEmptyListShouldBeReturned()
+        {
+            Assert.That(this.getAllResult, Is.Empty);
+        }
+
+        [Then(@"the category should be returned")]
+        public void ThenTheCategoryShouldBeReturned()
+        {
+            Assert.That(this.getOneResult, Is.EqualTo(this.theCategory));
+        }
+
+        [Then(@"nothing should be returned")]
+        public void ThenNothingShouldBeReturned()
+        {
+            Assert.That(this.getOneResult, Is.Null);
+        }
+    }
+}
---
+++ Solutions/SpecFlowTests.WhoCanHelpMe/Tasks/CategoryTasksFeatures/GetCategories.feature
@@ -0,0 +1,28 @@
+﻿Feature: Get categories
+    In order to add meaning to my tags
+    As a user
+    I want to get categories from the repository
+
+Scenario: Get all categories
+    Given I have category tasks available
+    And there are 3 categories in the repository
+    When I ask for all categories
+    Then the list of all categories should be returned
+
+Scenario: No categories are available
+    Given I have category tasks available
+    And there are 0 categories in the repository
+    When I ask for all categories
+    Then an empty list should be returned
+
+Scenario: Get a category by id
+    Given I have category tasks available
+    And there is a category with Id 3 in the repository
+    When I ask for the category with Id 3
+    Then the category should be returned
+
+Scenario: Get a category by id when there is no matching category
+    Given I have category tasks available
+    And there is no category with Id 3 in the repository
+    When I ask for the category with Id 3
+    Then nothing should be returned
